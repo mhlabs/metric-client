88494a4808d8efc874fa8e038f302c0f01f6e041
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2020-03-23 18:00:42 +0100

    chore: avoid duplicate domain tag if provided by tags

--- src/metricClient.js
+++ src/metricClient.js
@@ -5,9 +5,8 @@ function write(metric) {
   process.stdout.write(`${metric}\n`);
 }
 
-const increment = (type, count, tags, domain) => {
-  let metric = `${prefix}: ${stack} count ${type} ${count} domain:${domain ||
-    stack}`;
+const increment = (type, count, tags) => {
+  let metric = `${prefix}: ${stack} count ${type} ${count}`;
   if (tags && tags.length) {
     metric = `${metric} ${tags.join(' ')}`;
   }
--- src/metricClient.test.js
+++ src/metricClient.test.js
@@ -4,24 +4,12 @@ const metricClient = new MetricClient('virtual-test-stack', 'my.metrics');
 
 test('Increment', () => {
   const metric = metricClient.increment('product.search', 1, [
-    'identifier:the-one',
-    'team:my-team'
+    'domain:xyz',
+    'identifier:the_one',
+    'team:my_team'
   ]);
 
   expect(metric).toEqual(
-    'my.metrics: virtual-test-stack count product.search 1 domain:virtual-test-stack identifier:the-one team:my-team'
-  );
-});
-
-test('Increment with domain override', () => {
-  const metric = metricClient.increment(
-    'product.search',
-    1,
-    ['identifier:the-one', 'team:my-team'],
-    'my-domain'
-  );
-
-  expect(metric).toEqual(
-    'my.metrics: virtual-test-stack count product.search 1 domain:my-domain identifier:the-one team:my-team'
+    'my.metrics: virtual-test-stack count product.search 1 domain:xyz identifier:the_one team:my_team'
   );
 });
